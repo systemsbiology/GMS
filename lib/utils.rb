bf3179565bac510bc4382f2436e37e07fc45187e
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2011-12-15 14:38:14 -0800

    added function to read in the header of a file

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,3 +1,5 @@
+require 'open3'
+require 'time'
 
 def array_to_html_table(header, data)
     xm = Builder::XmlMarkup.new(:indent => 2)
@@ -12,4 +14,39 @@ def csvdir_exists
   if !File.exists?(CSVDIR) then
     Dir.mkdir(CSVDIR)
   end
+end
+
+def creation_time(file)
+   Time.parse(Open3.popen3("stat", 
+                           "-c",
+                           "%z", 
+                           file)[1].read)
+end
+
+# open up a file and parse the # comments into a hash.  Use bzcat if bzip'd or gunzip -c for gzip'd
+def file_header(file)
+  cmd = Array.new
+  if file.match(/\.bz2\z/) then
+    cmd = [ 'bzcat', file ]
+  elsif file.match(/\.gz\z/) then
+    cmd = [ 'gunzip', '-c', file ]
+  else
+    cmd = [ 'cat', file ]
+  end
+
+  header = Hash.new
+  Open3.popen3(*cmd) do |stdin, stdout, stderr|
+    stdout.each do |line|
+      if line.match('^#') then
+        line.gsub!(/#/, '')
+	line.strip! # remove newline at the end
+        (key,value) = line.split(/\t/)
+        header[key] = value
+      else
+        break
+      end
+    end
+  end
+
+  return header
 end
