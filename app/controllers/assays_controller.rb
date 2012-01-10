0efa1e1f706e91e4da2f2533b739114b571a4e10
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-01-10 12:27:08 -0800

    Removed nobr and added a style to tds for nowrap

--- app/controllers/assays_controller.rb
+++ app/controllers/assays_controller.rb
@@ -2,7 +2,7 @@ class AssaysController < ApplicationController
   # GET /assays
   # GET /assays.xml
   def index
-    @assays = Assay.has_pedigree(params[:pedigree_filter]).has_name(params[:assay_name])
+    @assays = Assay.has_pedigree(params[:pedigree_filter]).has_name(params[:assay_name]).paginate :page => params[:page], :per_page => 100
 
     respond_to do |format|
       format.html # index.html.erb
