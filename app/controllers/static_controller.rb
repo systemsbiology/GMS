9b7e2067aec92eec16941ab322e30304da8fbd01
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-02-09 14:28:08 -0800

    Added a controller to handle static pages, static pages in the view for that controller, images that explain the upload formats, and an example spreadsheet of the FGG Manifest format.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+
+  #static page that describes the FGG manifest format
+  def fgg_manifest
+  end
+
+  #static page that describes the CGI manifest format
+  def cgi_manifest
+  end
+
+end
