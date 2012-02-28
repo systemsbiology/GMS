63f9e5c1c3965b416499c81240bbd5f6efad2aa7
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-02-27 17:42:22 -0800

    urg

--- config/deploy.rb
+++ config/deploy.rb
@@ -192,7 +192,7 @@ server "bobama.systemsbiology.net", :app, :web, :db, :primary => true
    task :trust_rvmrc do
      puts "current release is #{current_release} release_path is #{release_path}"
 #     run "rvm rvmrc trust #{current_release}"
-     run "rvm rvmrc trust #{current_release}", :hosts => :primary
+     run "rvm rvmrc trust #{current_release}", :shell => "/bin/bash"
    end
  end
 
