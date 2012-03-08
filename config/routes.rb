e1c16c71daee1fe8ab62d3bbc70042b0258e5e1f
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-03-07 16:55:46 -0800

    added founders method

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Gms::Application.routes.draw do
   match "pedigrees/all_pedigree_files", :to => "pedigrees#all_pedigree_files", :as => "all_pedigree_files"
   match "pedigrees/pedigree_datastore", :to => "pedigrees#pedigree_datastore", :as => "pedigree_datastore"
   match "pedigrees/export_madeline_table/(:id)", :to => "pedigrees#export_madeline_table", :as => "export_madeline_table"
+  match "pedigrees/founders/(:id)", :to => "pedigrees#founders", :as => "founders"
   resources :pedigrees
   resources :studies
   match "/fgg_manifest", :to => "static#fgg_manifest"
