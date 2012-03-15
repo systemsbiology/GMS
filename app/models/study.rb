52228701573de594c5cd31e9bf9936ca76a5dc7e
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-03-14 17:23:23 -0700

    make sure there are no spaces in the study tag

--- app/models/study.rb
+++ app/models/study.rb
@@ -3,4 +3,13 @@ class Study < ActiveRecord::Base
   auto_strip_attributes :name, :tag, :collaborator, :collaborating_institution, :lead, :description, :contact
   validates_presence_of :name, :tag, :collaborator, :collaborating_institution
   validates_uniqueness_of :name, :tag
+
+  after_save :check_study_tag
+
+  def check_study_tag
+    if !self.tag.nil? and self.tag.match(/ /) then
+      tag = self.tag.gsub!(/ /,"_")
+      self.update_attributes(:tag => tag)
+    end
+  end
 end
