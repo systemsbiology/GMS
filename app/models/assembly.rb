179326e77e0ea8b3edc6b31b3c1cec8e9059f7e9
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-07-24 11:45:30 -0700

    updated has_pedigree to be standard

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -14,7 +14,13 @@ class Assembly < ActiveRecord::Base
 
   scope :has_pedigree, lambda { |pedigree|
     unless pedigree.blank?
+      if pedigree.kind_of?(Array) then
+        pedigree_id = pedigree[0]
+      elsif pedigree.kind_of?(Hash) then
       pedigree_id = pedigree[:id]
+      else
+      pedigree_id = pedigree.to_i
+      end
       unless pedigree_id.blank?
         { :include => { :assay => { :sample => { :person => :pedigree } } },
           :conditions => [ 'pedigrees.id = ?', pedigree_id]
