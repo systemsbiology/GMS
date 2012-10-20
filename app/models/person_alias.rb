e857d2f9e70dd70a11a503b8d53b2e0cd9dd63fd
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-10-19 18:59:35 -0700

    Updating name to alias_type and removing name

--- app/models/person_alias.rb
+++ app/models/person_alias.rb
@@ -2,5 +2,5 @@ class PersonAlias < ActiveRecord::Base
   belongs_to :person
 
   auto_strip_attributes :value
-  validates_presence_of :person_id, :name, :value, :alias_type
+  validates_presence_of :person_id, :value, :alias_type
 end
