1c2d1e91d8d6d4032f332355e8d58ee4d5dd6d50
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2012-10-24 11:06:18 -0700

    adding support for a tag for a phenotype

--- app/models/phenotype.rb
+++ app/models/phenotype.rb
@@ -3,7 +3,7 @@ class Phenotype < ActiveRecord::Base
   has_many :traits, :dependent => :destroy
   belongs_to :disease
 
-  auto_strip_attributes :name, :description
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  auto_strip_attributes :name, :tag, :description
+  validates_presence_of :name, :tag
+  validates_uniqueness_of :name, :tag
 end
