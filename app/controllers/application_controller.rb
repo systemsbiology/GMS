430af2a66a5ffa51312ad1f50d143203edda1c5e
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2013-01-11 15:06:03 -0800

    fixed wrapIt loading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ require 'will_paginate/array'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include ApplicationHelper  
-
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 
-
 def wrapIt(text, width=80, string="<wbr />")
   text.midsub(%r{(\A|</pre>)(.*?)(\Z|<pre(?: .+?)?>)}im) do |outside_pre|  # Not inside <pre></pre>
     outside_pre.midsub(%r{(\A|>)(.*?)(\Z|<)}m) do |outside_tags|  # Not inside < >, either
