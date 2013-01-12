a7a92eea8d0610cf5b60854c5bba36481cdb5447
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2013-01-11 17:11:54 -0800

    removed helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
 
-def wrapIt(text, width=80, string="<wbr />")
-  text.midsub(%r{(\A|</pre>)(.*?)(\Z|<pre(?: .+?)?>)}im) do |outside_pre|  # Not inside <pre></pre>
-    outside_pre.midsub(%r{(\A|>)(.*?)(\Z|<)}m) do |outside_tags|  # Not inside < >, either
-      outside_tags.gsub(/(\S{#{width}})(?=\S)/) { "#$1#{string}" }
-    end
-  end
-end
-
 end
