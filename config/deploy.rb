0b316f6337848a4947b09e684a119a9015bb26db
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2013-03-14 16:30:26 -0700

    update rvm settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :whenever_identifier, defer { "#{application}_#{environment}" }
 set :rail_env, "production"
 set :rvm_bin_path, "/u5/tools/rvm/bin"
 set :rvm_scripts_path, "/u5/tools/rvm/bin"
+set :rvm_path, "/u5/tools/rvm"
 set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 set :rvm_install_ruby_params, '--1.9'
 set :rvm_install_pkgs, %w[libyaml openssl]
