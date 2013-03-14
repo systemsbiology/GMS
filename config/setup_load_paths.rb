e602d906ea4e6fbcbf90bedd477a2231f09bb509
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2013-03-14 16:20:05 -0700

    new path?

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,9 +1,9 @@
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
-    gems_path    = ENV['MY_RUBY_HOME'].split(/@/)[0].sub(/rubies/,'gems')
-    ENV['GEM_PATH'] = "#{gems_path}:#{gems_path}@global"
-#    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-#    rvm_lib_path = File.join(rvm_path, 'lib')
+#    gems_path    = ENV['MY_RUBY_HOME'].split(/@/)[0].sub(/rubies/,'gems')
+#    ENV['GEM_PATH'] = "#{gems_path}:#{gems_path}@global"
+    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
     #$LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
