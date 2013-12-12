31ab3317fe876c5c30f27748d38be9a50e6523d0
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2013-12-12 14:21:23 -0800

    moved code in ingenuity.rake into ingenuity.rb so that it will be able to be used by the rake task and the samples controller.  added new ingenuity.rake to use the library

--- lib/ingenuity.rb
+++ lib/ingenuity.rb
@@ -1,16 +1,12 @@
-require 'fileutils'
+require 'tmpdir'
 
-namespace :ingenuity do
-  
-  desc "Take a file of Ingenuity samples and determine samples in GMS that haven't been uploaded"
-  task :find_missing_ingenuity, [:file] => :environment do |t, args|
-    puts "file is #{args[:file]}"
+def check_ingenuity(filename)
 
     skips = ['diversity_P1', 'YRI_trio_P1', 'LVNC_P1', 'PUR_trio_P1', 'ceph_1463']
-
+    Rails.logger.debug "filename is #{filename}"
     ingenuity = Array.new
     begin
-        File.open(args[:file], "r") do |f|
+        File.open(filename, "r") do |f|
         while (line = f.gets)
 	        # columns should be Barcode (assembly id), Name (some pseudo random well number and pedigree number?),
 	        # description (roughly space delimited), subject id (GS0XXXX-DNA_XXX), Files (semicolon delmited)
@@ -20,10 +16,10 @@ namespace :ingenuity do
 	        next if assembly_id.match("Barcode")
             assembly_id.gsub!("_masterVar","")
             if (!assembly_id.match("-ASM")) then
-                puts "adding ASM to end"
+#                Rails.logger.debug "adding ASM to end"
                 assembly_id = assembly_id+'-ASM'
             end
-            puts "finding assembly #{assembly_id}"
+#            Rails.logger.debug "finding assembly #{assembly_id}"
             asm = Assembly.find_by_name(assembly_id)
 	        asm = Assembly.find(:all, :conditions => ["name LIKE ?", "#{assembly_id}%"]) if asm.nil?
 	        raise "More than one assembly found with #{assembly_id}" if asm.instance_of?(Array) && asm.size > 1
@@ -35,41 +31,44 @@ namespace :ingenuity do
 	            # sample id in the name column
 	            sample = Sample.find_by_sample_vendor_id(columns[1])
 	            if sample.nil? then
-	                puts "Didn't find #{assembly_id} or #{columns[1]}"
+	                Rails.logger.debug "Didn't find #{assembly_id} or #{columns[1]}"
 	            else 
-#	                puts "found sample #{sample.inspect}"
+#	                Rails.logger.debug "found sample #{sample.inspect}"
                     # getting the last assembly should be proper since that should be the current one
                     asm = sample.assays.last.assemblies.last 
-#	                puts "found asm #{asm.inspect} for sample #{sample.inspect}"
+#	                Rails.logger.debug "found asm #{asm.inspect} for sample #{sample.inspect}"
 	                ingenuity.push(asm)
 	            end
 	        else
-#	            puts "found asm #{asm.inspect}"
+#	            Rails.logger.debug "found asm #{asm.inspect}"
                 ingenuity.push(asm)
             end
         end
     end
     rescue => err
-      puts "Exception: #{err}"
+      Rails.logger.debug "Exception: #{err}"
     end
 
-    puts "size is #{ingenuity.size}"
-    outfile = File.open("ingenuity_add.txt", "w+")
+#    Rails.logger.debug "size is #{ingenuity.size}"
+    timestamp = Time.new.strftime("%Y%m%d%H%M%S")
+    outfilename = "ingenuity_add_#{timestamp}.txt"
+    path = File.join(Dir::tmpdir, outfilename)
+    outfile = File.open(path, "w+")
     outfile.write("Assembly Name\tAssembly Location\tPedigree\tSample Vendor ID\tISB Person ID\tCollaborator ID\n")
     Assembly.all.each do |assembly|
       if ingenuity.include? assembly then
-      	puts "skipping assembly because it's included in list #{assembly.inspect}"
-	next
+#      	Rails.logger.debug "skipping assembly because it's included in list #{assembly.inspect}"
+	    next
       end
-#      puts "Need to add #{assembly.inspect} to ingenuity"
-      puts "assembly #{assembly.inspect} does not have an assay association!!" unless assembly.assay
+#      Rails.logger.debug "Need to add #{assembly.inspect} to ingenuity"
+      Rails.logger.debug "assembly #{assembly.inspect} does not have an assay association!!" unless assembly.assay
       next unless assembly.assay
-      puts "assembly #{assembly.inspect} assay #{assembly.assay.inspect} does not have a sample association!!" unless assembly.assay.sample
+      Rails.logger.debug "assembly #{assembly.inspect} assay #{assembly.assay.inspect} does not have a sample association!!" unless assembly.assay.sample
       next unless assembly.assay.sample
       next if skips.include? assembly.assay.sample.person.pedigree.tag
       outfile.write("#{assembly.name}\t#{assembly.location}\t#{assembly.assay.sample.person.pedigree.tag}\t#{assembly.assay.sample.sample_vendor_id}\t#{assembly.assay.sample.person.isb_person_id}\t#{assembly.assay.sample.person.collaborator_id}\n")
     end
     outfile.close
-  end
-
+    return path
 end
+
