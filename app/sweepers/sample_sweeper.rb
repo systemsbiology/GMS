12ecfa6e27eeb398ac2c058a61d5034fc47c27ab
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2014-01-14 20:51:40 -0800

    dunno why it needs this but it does?

--- app/sweepers/sample_sweeper.rb
+++ app/sweepers/sample_sweeper.rb
@@ -2,7 +2,8 @@ class SampleSweeper < ActionController::Caching::Sweeper
     observe Sample
 
     # expire people ped_info
-    def after_save
+    def after_save(sample)
+        Rails.logger.debug("sample in after_save? #{sample.inspect}")
         expire_action(:controller => 'people', :action => :ped_info)
     end
 end
