f8bb00752912414c8b376eb0a44e10536cfb1987
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2014-02-19 15:47:02 -0800

    updated to sweep after assembly save because pedigree checkin script saves assemblies and then assembly_files are added via trigger

--- app/sweepers/assembly_files_sweeper.rb
+++
@@ -1,8 +0,0 @@
-class AssemblyFilesSweeper < ActionController::Caching::Sweeper
-    observe AssemblyFile
-
-    # expire assembly_files ped_info
-    def after_save
-        expire_action(:controller => 'assembly_files', :action => :ped_info)
-    end
-end
