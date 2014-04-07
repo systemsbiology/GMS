9838ce6b243c406a179da06eead9e3a64ea0382a
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2014-04-07 13:44:33 -0700

    remove devise/cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 require 'will_paginate/array'
 
 class ApplicationController < ActionController::Base
-  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
-    render :text => exception, :status => 500
-  end
-  protect_from_forgery
+  #rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+  #  render :text => exception, :status => 500
+  #end
+  #protect_from_forgery
 
+  #rescue_from CanCan::AccessDenied do |exception|
+  #  flash[:error] = "Access Denied"
+  #  redirect_to root_url
+  #end
 end
