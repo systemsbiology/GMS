8c4cc7bba682a18159d23d18e80d0d1dcbf87980
Denise Mauldin <dmauldin@mendel.systemsbiology.net>
2015-04-07 16:40:13 -0700

    add expire people

--- app/sweepers/assembly_files_sweeper.rb
+++ app/sweepers/assembly_files_sweeper.rb
@@ -4,5 +4,6 @@ class AssemblyFilesSweeper < ActionController::Caching::Sweeper
     # expire assembly_file ped_info
     def after_save(assembly_file)
         expire_action(:controller => '/assembly_files', :action => 'ped_info', :format => 'json')
+        expire_action(:controller => '/people', :action => 'ped_info', :format => 'json')
     end
 end
